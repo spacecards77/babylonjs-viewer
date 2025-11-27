3f9277395ba15977c46fa5dd3b7854c643347d64
dmaiseyenka <dmitriymois@gmail.com>
2025-11-27 10:19:11 +0300

    Настройка автоматического деплоя на github pages.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
     return {
+        base: command === 'build' ? '/babylonjs-viewer/' : './',
         resolve: {
             alias: {
                 'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
