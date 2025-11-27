d0ca78bfa59d326e4eb40e3da13ba3c108af669c
dmaiseyenka <dmitriymois@gmail.com>
2025-11-27 14:47:46 +0300

    Новая камера для вращения вокруг объекта

--- src/services/SceneService.ts
+++ src/services/SceneService.ts
@@ -10,8 +10,8 @@ export class SceneService {
     createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
         var scene = new BABYLON.Scene(engine);
 
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
+        var camera = new BABYLON.ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 2, 20, new BABYLON.Vector3(20, -20, 20), scene);
+        camera.setTarget(new BABYLON.Vector3(20, 20, 10));
         camera.attachControl(canvas, true);
 
         var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
@@ -37,48 +37,11 @@ export class SceneService {
         button.onPointerUpObservable.add(() => {
             UploadService.uploadJson((jsonContent) => {
                 const construction = jsonService.deserialize(jsonContent);
-                console.log('Deserialized construction:', construction);
+                //console.log('Deserialized construction:', construction);
 
                 drawService.drawConstruction(construction);
             });
         });
-
-        /*// Our built-in 'sphere' shape.
-        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-        // Move the sphere upward 1/2 its height
-        let startPos = 2;
-        sphere.position.y = startPos;
-
-        // Our built-in 'ground' shape.
-        var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-        var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-        groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5); // RGB for a greenish color
-        ground.material = groundMaterial;
-        groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-        //groundMaterial.bumpTexture.level = 0.125;
-
-
-        var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-        redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
-        sphere.material = redMaterial;
-
-        var sphereVelocity = 0;
-        var gravity = 0.009;
-        var reboundLoss = 0.1;
-
-        scene.registerBeforeRender(() => {
-            sphereVelocity += gravity;
-            let newY = sphere.position.y - sphereVelocity;
-            sphere.position.y -= sphereVelocity
-            if (newY < 1) {
-                sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-                newY = 1;
-            }
-            sphere.position.y = newY;
-            if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-                sphere.position.y = startPos++;
-            }
-        });*/
 
         return scene;
     }
